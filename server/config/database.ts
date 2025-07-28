37cca104c778b537cd4a2ff90e776132e65103a8
anibal andrade <59257555+androso@users.noreply.github.com>
2025-07-28 04:09:16 +0000

    using config store for env variables

--- server/config/database.ts
+++ server/config/database.ts
@@ -2,22 +2,11 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from "@shared/schema";
 import "dotenv/config";
-
-if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error(
-    "TURSO_DATABASE_URL must be set. Did you forget to provision a Turso database?",
-  );
-}
-
-if (!process.env.TURSO_AUTH_TOKEN) {
-  throw new Error(
-    "TURSO_AUTH_TOKEN must be set. Did you forget to set your Turso auth token?",
-  );
-}
+import { config } from "../config";
 
 export const client = createClient({
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: config.db.url,
+  authToken: config.db.authToken,
 });
 
 export const db = drizzle(client, { schema });
